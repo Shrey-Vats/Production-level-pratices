71fd2caccceb475ac23c6694baf4e8251ec29a6b
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-09 03:46:46 +0530

    update: user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -25,7 +25,7 @@ export interface User extends Document {
     verifyCodeExpire: Date;
     isVerified: boolean;
     isAcceptingMessage: boolean;
-    message: Message[]
+    messages: Message[]
 }
 
 const UserSchema: Schema<User> = new Schema({
@@ -62,7 +62,7 @@ const UserSchema: Schema<User> = new Schema({
     type: Boolean,
     default: true,
   },
-  message: [MessageSchema],
+  messages: [MessageSchema],
 });
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
