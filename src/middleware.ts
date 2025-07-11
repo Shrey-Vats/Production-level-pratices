7485bf267e34c700aedd295cd7da3b20206f86e2
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-11 13:49:28 +0530

    update: auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,8 +23,10 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
+    if(!token && url.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
 
-  return NextResponse.redirect(new URL("/home", request.url));
 }
 
 // See "Matching Paths" below to learn more
